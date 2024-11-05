5f6df296cbff156fd7c9bb8f4ff92820b224bc1d
Daniels Satcs <shatz.dan@gmail.com>
2024-11-05 16:48:54 -0500

    analysis

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+"""Flask server for emotion detection"""
+
+from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
-from flask import request
 
 app = Flask(__name__)
 
+
 @app.route("/emotionDetector")
 def emotion():
+    """Api endpoint for emotion detection"""
     result = emotion_detector(request.args.get("textToAnalyze"))
     if not result["dominant_emotion"]:
         return "Invalid text! Please try again!"
@@ -18,4 +21,6 @@ def emotion():
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    """UI route"""
+    return render_template('index.html')
+   
