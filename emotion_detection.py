eb88e3148d97d5314a1a8081a84575b0a31de532
Daniels Satcs <shatz.dan@gmail.com>
2024-11-05 22:43:38 +0200

    Task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,11 @@
 import requests
 
 def emotion_detector(text_to_analyze):
-    return requests.post(
+    json = requests.post(
         "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict",
         headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"},
         json = { "raw_document": { "text": text_to_analyze } }
-    ).text
+    ).json()
+    emotions = json["emotionPredictions"][0]["emotion"]
+    emotions["dominant_emotion"] = max(emotions, key=lambda d: d[1])
+    return emotions
